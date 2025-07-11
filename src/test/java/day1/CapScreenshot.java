4f1af636524de8edff4ad91a91d60086caa561e9
kanakasai99 <kanakasaimereddy99@gmail.com>
2025-07-11 16:08:30 +0530

    selenium code update

--- src/test/java/day1/CapScreenshot.java
+++ src/test/java/day1/CapScreenshot.java
@@ -1,8 +1,11 @@
  package day1;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+//import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -10,22 +13,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class CapScreenshot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 	
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		driver.get("https://demo.nopcommerce.com/");
+		//driver.get("https://demo.nopcommerce.com/");
+		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
 		
 		//1) full page screenshot
 		TakesScreenshot ts=(TakesScreenshot)driver;
 		File sourcefile=ts.getScreenshotAs(OutputType.FILE);
-		File targetfile=new File(System.getProperty("user.dir")+"\\Screenshot\\fullpage.png");
-		sourcefile.renameTo(targetfile); // copy sourcefile to target file
-		
+		File targetfile=new File(System.getProperty("user.dir")+"\\Screenshot\\flipcartmainpage.png");
+	//	sourcefile.renameTo(targetfile); // copy sourcefile to target file
+		FileUtils.copyFile(sourcefile, targetfile);
 		
 		//2) capture the screenshot of specific section
 		/*WebElement featuredProducts=driver.findElement(By.xpath("//div[@class='product-grid home-page-product-grid']"));
@@ -41,7 +46,7 @@ public class CapScreenshot {
 		File targetfile=new File(System.getProperty("user.dir")+"\\screenshots\\logo.png");
 		sourcefile.renameTo(targetfile); // copy sourcefile to target file*/
 		
-		//driver.quit();
+		driver.quit();
 		
 		
 	
--- src/test/java/day1/XpathDemo.java
+++ src/test/java/day1/XpathDemo.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XpathDemo {
 
-	public static void main(String[] args) {
+	/**/public static void main(String[] args) {
 		
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://demo.opencart.com/");
